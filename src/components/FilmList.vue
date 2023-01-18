a94d0473015c1cbf62456edfa9fb6029f4e6ddb1
mattiabenelli <benellimattia0@gmail.com>
2023-01-18 12:02:51 +0100

    aggiunte serie

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -16,37 +16,54 @@ export default {
     },
     mounted(){
         this.getFilm();
+        // this.getSeries();
+        // this.getLanguage();
     },
     methods:{
         getFilm(){
-            let myUrl = store.url + store.searchText
+            let myUrlMovies = store.urlFilm + store.searchText
 
-            axios.get(myUrl).then((response) => {
+            axios.get(myUrlMovies).then((response) => {
                 store.FilmList = response.data.results
             })
+            let myUrlSeries = store.urlSeries + store.searchText
+
+            axios.get(myUrlSeries).then((response) => {
+                store.SeriesList = response.data.results
+            })
         },
-        getLanguage(){
-            store.FilmList.original_language = language;
-            console.log(language.toUpperCase())
-        }
+        // getLanguage(){
+        //     let language = (store.FilmList.original_language).toUpperCase();
+        //     console.log(language)
+        // }
     },
 }
 </script>
 
 <template lang="">
-    <AppSearch @search="getFilm, getLanguage" />
+    <AppSearch @search="getFilm" />
     <div class="card" v-for="(item,index) in store.FilmList" :key="index">
-            <div>{{ item.title }}</div>
-            <div>{{ item.original_title }}</div>
-            <div>{{ item.original_language }}</div>
-            <div>{{ item.vote_average }}</div>
-            <!-- <img src="`https://flagsapi.com/BE/flat/64.png`"> -->
-        </div>
+        <div>{{ item.title }}</div>
+        <div>{{ item.original_title }}</div>
+        <div>{{ item.original_language }}</div>
+        <div>{{ item.vote_average }}</div>
+        <img :src=`"https://unpkg.com/language-icons/icons/${{ item.original_language }}.svg"`>
+    </div>
+    <div class="card series" v-for="(item,index) in store.SeriesList" :key="index">
+        <div>{{ item.name }}</div>
+        <div>{{ item.original_name}}</div>
+        <div>{{ item.original_language }}</div>
+        <div>{{ item.vote_average }}</div>
+        <!-- <img src="`https://flagsapi.com/${language}/flat/64.png`"> -->
+    </div>
 </template>
 
 <style lang="scss">
     .card{
         display: flex;
-        gap: 20px
+        gap: 20px;
+    }
+    .series{
+        color: red;
     }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { reactive } from "vue";
 export const store = reactive({
-    url: "https://api.themoviedb.org/3/search/movie?api_key=62237f7daf774ec5af10f8e4a4de5ed5&query=",
+    urlFilm: "https://api.themoviedb.org/3/search/movie?api_key=62237f7daf774ec5af10f8e4a4de5ed5&query=",
+    urlSeries: "https://api.themoviedb.org/3/search/tv?api_key=62237f7daf774ec5af10f8e4a4de5ed5&query=",
     ApiFlag: "",
     searchText: "star",
-    FilmList: []
+    FilmList: [],
+    SeriesList: []
 })
 {/* <img :src="https://www.countryflagicons.com/FLAT/64/${getFlag(details)}.png"> */ }
