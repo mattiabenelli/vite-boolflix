91ea64eaebe0522718d877195913bd25bb054914
mattiabenelli <benellimattia0@gmail.com>
2023-01-18 11:15:31 +0100

    problema lingua maiuscolo

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -25,17 +25,22 @@ export default {
                 store.FilmList = response.data.results
             })
         },
+        getLanguage(){
+            store.FilmList.original_language = language;
+            console.log(language.toUpperCase())
+        }
     },
 }
 </script>
 
 <template lang="">
-    <AppSearch @search="getFilm" />
+    <AppSearch @search="getFilm, getLanguage" />
     <div class="card" v-for="(item,index) in store.FilmList" :key="index">
             <div>{{ item.title }}</div>
             <div>{{ item.original_title }}</div>
             <div>{{ item.original_language }}</div>
             <div>{{ item.vote_average }}</div>
+            <!-- <img src="`https://flagsapi.com/BE/flat/64.png`"> -->
         </div>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import { reactive } from "vue";
 export const store = reactive({
     url: "https://api.themoviedb.org/3/search/movie?api_key=62237f7daf774ec5af10f8e4a4de5ed5&query=",
+    ApiFlag: "",
     searchText: "star",
     FilmList: []
 })
+{/* <img :src="https://www.countryflagicons.com/FLAT/64/${getFlag(details)}.png"> */ }
